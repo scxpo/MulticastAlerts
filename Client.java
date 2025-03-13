4f7f8a95c298e6666d78eb50dce09f7a93e57a65
riccardo <riccardo@gmail.com>
2025-03-13 16:20:24 +0100

    creazione client

--- Client.java
+++ Client.java
@@ -1,3 +1,26 @@
+import java.net.*;
+import java.io.*;
+
 public class Client {
-    
+    private static final String MULTICAST_ADDRESS = "230.0.0.1";
+    private static final int PORT = 4446;
+
+    public static void main(String[] args) {
+        try (MulticastSocket socket = new MulticastSocket(PORT)) {
+            InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
+            socket.joinGroup(group);
+            
+            System.out.println("Client in ascolto sui messaggi di emergenza...");
+            byte[] buffer = new byte[256];
+            
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                String received = new String(packet.getData(), 0, packet.getLength());
+                System.out.println("Messaggio ricevuto: " + received);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
